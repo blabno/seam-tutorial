b485b7d45c3309b4f4f84fc54a32a01fb46c5e6b
Bernard Labno <bernard.labno@pjwstk.edu.pl>
2013-02-14 15:35:02 +0100

    Locale selector - simpler

--- src/main/java/pl/itcrowd/tutorials/itc/LocaleSelector.java
+++ src/main/java/pl/itcrowd/tutorials/itc/LocaleSelector.java
@@ -13,31 +13,13 @@ import java.util.Locale;
 @RequestScoped
 public class LocaleSelector {
 
-    private Locale locale;
-
     @Inject
     @Alter
     @Client
     private Event<Locale> localeEvent;
 
-    public LocaleSelector()
-    {
-    }
-
-    @Inject
-    public LocaleSelector(@Client Locale locale)
-    {
-        this.locale = locale;
-    }
-
-    public Locale getLocale()
-    {
-        return locale;
-    }
-
     public void setLocale(Locale locale)
     {
-        this.locale = locale;
-        localeEvent.fire(this.locale);
+        localeEvent.fire(locale);
     }
 }
