04e2c68bc73b6ff39adf21eba9d5112d805685d0
Bernard Labno <bernard.labno@pjwstk.edu.pl>
2013-02-15 11:22:10 +0100

    Security - reuse authorizer on facelets.

--- src/main/java/pl/itcrowd/tutorials/itc/security/Authorizer.java
+++ src/main/java/pl/itcrowd/tutorials/itc/security/Authorizer.java
@@ -3,11 +3,27 @@ package pl.itcrowd.tutorials.itc.security;
 import org.jboss.seam.security.Identity;
 import org.jboss.seam.security.annotations.Secures;
 
+import javax.inject.Inject;
+import javax.inject.Named;
+
+@Named
 public class Authorizer {
+
+    private Identity identity;
+
+    public Authorizer()
+    {
+    }
+
+    @Inject
+    public Authorizer(Identity identity)
+    {
+        this.identity = identity;
+    }
 
     @Secures
     @Admin
-    public boolean isAdmin(Identity identity)
+    public boolean isAdmin()
     {
         return identity.hasRole("admin", "USERS", "GROUP");
     }
