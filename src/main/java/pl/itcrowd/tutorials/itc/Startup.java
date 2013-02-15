0aac7487ea949896682346a478a6b7a08f750f96
Bernard Labno <bernard.labno@pjwstk.edu.pl>
2013-02-15 16:11:52 +0100

    Mail - simple mail using mail session configured by app

--- src/main/java/pl/itcrowd/tutorials/itc/Startup.java
+++ src/main/java/pl/itcrowd/tutorials/itc/Startup.java
@@ -1,5 +1,6 @@
 package pl.itcrowd.tutorials.itc;
 
+import org.jboss.seam.mail.core.MailConfig;
 import org.jboss.seam.transaction.Transactional;
 import org.jboss.solder.servlet.WebApplication;
 import org.jboss.solder.servlet.event.Initialized;
@@ -16,11 +17,21 @@ public class Startup {
     @Inject
     private EntityManager entityManager;
 
+    @Inject
+    private MailConfig mailConfig;
+
     @Transactional
     public void seed(@Observes @Initialized WebApplication webApplication)
     {
         entityManager.persist(new User("bernard@itcrowd.pl", "aaaaa", "admin"));
         entityManager.persist(new User("jack@itcrowd.pl", "aaaaa", "user"));
         entityManager.flush();
+
+        mailConfig.setAuth(true);
+        mailConfig.setEnableSsl(true);
+        mailConfig.setPassword("");//provide your email account password
+        mailConfig.setServerHost("");//provide your email account mail server host name
+        mailConfig.setServerPort(465);//provide your email account mail server port
+        mailConfig.setUsername(""); //provide your email account username (for gmail it may be same as email)
     }
 }
